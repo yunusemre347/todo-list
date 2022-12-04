a01468992a3e359ecc535b5fd819e392f3965d38
yunusemre347 <yunusemreyilmaz347@gmail.com>
2022-12-04 17:54:36 +0300

    Deployed website

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ function App() {
   return (
     <div className="App">
         <Tasks/>
-        
+        <div className='notes'> <p>Notes: this is a todo list made by me. I did it to practice react. No css, i didnt think it was necessary for this. yunusemre347@gmail.com</p> </div>
     </div>
+
+    
   );
 }
 
